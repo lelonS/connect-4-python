00c8c8b73e4bae38a878f428a82a126cd482b4aa
lelonS <35070147+lelonS@users.noreply.github.com>
2022-10-31 19:13:52 +0100

    Fix bug al cols same instance

--- classes/connect4.py
+++ classes/connect4.py
@@ -7,7 +7,8 @@ class ConnectFour:
     def __init__(self, total_cols: int, total_rows: int) -> None:
         self.total_cols = total_cols
         self.total_rows = total_rows
-        self.board = [[-1]*total_rows]*total_cols  # put "-1" in every empty tile on board.
+        # put "-1" in every empty tile on board.
+        self.board = [[-1] * total_rows for _ in range(total_cols)]
 
     def is_legal_move(self, col: int) -> bool:
         return self.board[col][-1] == -1
