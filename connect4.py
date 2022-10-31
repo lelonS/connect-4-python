8511f2b02477ba0daed57effb2a447e9bd784253
lelonS <35070147+lelonS@users.noreply.github.com>
2022-10-31 15:15:48 +0100

    Update connect4.py

--- connect4.py
+++ connect4.py
@@ -1,2 +1,15 @@
 class ConnectFour:
-    pass
+    board: list[list[int]]  # board[col][row]
+    turn: int = 0
+
+    def __init__(self, size_x, size_y) -> None:
+        pass
+
+    def is_legal_move():
+        pass
+
+    def make_move():
+        pass
+
+    def check_win():
+        pass
