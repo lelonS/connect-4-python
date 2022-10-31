f2ea60fa0e4a5a00db7c92983961c8f64af419fc
Teddymannen <teddy_kross@hotmail.com>
2022-10-31 16:14:37 +0100

    Fixed a bug in row creation.

--- classes/connect4.py
+++ classes/connect4.py
@@ -2,12 +2,12 @@ class ConnectFour:
     board: list[list[int]]  # board[col][row]
     turn: int = 0
     total_cols: int
-    total_cols: int
+    total_rows: int
 
     def __init__(self, total_cols: int, total_rows: int) -> None:
         self.total_cols = total_cols
-        self.total_cols = total_rows
-        self.board = [[-1]*total_rows]*total_cols
+        self.total_rows = total_rows
+        self.board = [[-1]*total_rows]*total_cols  # put "-1" in every empty tile on board.
 
     def is_legal_move(self, col: int) -> bool:
         return self.board[col][-1] == -1
