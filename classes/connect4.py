18120e7c3276568bb6b7609d2ba37c14cca0c179
Teddymannen <teddy_kross@hotmail.com>
2022-10-31 15:59:30 +0100

    Add function is_legal_move

--- classes/connect4.py
+++ classes/connect4.py
@@ -5,8 +5,8 @@ class ConnectFour:
     def __init__(self, size_x, size_y) -> None:
         pass
 
-    def is_legal_move():
-        pass
+    def is_legal_move(self, col: int) -> bool:
+        return self.board[col][-1] == -1
 
     def make_move():
         pass
