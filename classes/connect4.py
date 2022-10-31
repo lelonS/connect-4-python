6043c4246b6494b8f0bea2bafd108a4c57d324d5
lelonS <35070147+lelonS@users.noreply.github.com>
2022-10-31 15:39:32 +0100

    Add __init__

--- classes/connect4.py
+++ classes/connect4.py
@@ -5,7 +5,14 @@ class ConnectFour:
     amount_rows: int
 
     def __init__(self, amount_cols: int, amount_rows: int) -> None:
-        pass
+        self.amount_cols = amount_cols
+        self.amount_rows = amount_rows
+        self.board = []
+        for _ in range(amount_cols):
+            col = []
+            for _ in range(amount_rows):
+                col.append(-1)  # Empty tile represented with -1
+            self.board.append(col)
 
     def is_legal_move():
         pass
