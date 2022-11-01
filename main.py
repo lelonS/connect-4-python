a6fd083a97704db1c9490c8a2457caab18c92bb9
lelonS <35070147+lelonS@users.noreply.github.com>
2022-11-01 10:31:55 +0100

    Create pygame screen

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
+import pygame
+
+
+pygame.init()
+
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+
+WIDTH = 900
+HEIGHT = 600
+SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Connect4")
+
+
 def main():
     pass
 
