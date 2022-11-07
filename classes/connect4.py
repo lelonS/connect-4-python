42544e2f480254327b8cee356f0bab34dac41c0a
Teddymannen <teddy_kross@hotmail.com>
2022-11-07 13:27:48 +0100

    Created a function to check if you won in any direction.

--- classes/connect4.py
+++ classes/connect4.py
@@ -21,6 +21,66 @@ class ConnectFour:
             # Change turn 0 -> 1 -> 0
             # self.turn = 1 if self.turn == 0 else 0
             self.turn = (self.turn + 1) % 2
+            print(self.check_win_at(col, len(self.board[col]) - 1))
 
-    def check_win(self):
-        pass
+    def check_direction_win(self, col: int, row: int, direction: tuple[int, int]) -> bool:  # Check horizontal win
+        current_plr_pos = self.board[col][row]
+        first_check = 0
+        second_check = 0
+
+        for i in range(1, 4):
+            col_check = col + i * direction[0]
+            row_check = row + i * direction[1]
+            if 0 > col_check or col_check >= self.total_cols:
+                break
+            if 0 > row_check or row_check >= len(self.board[col_check]):
+                break
+            if current_plr_pos == self.board[col_check][row_check]:
+                first_check += 1
+            else:
+                break
+
+        for i in range(1, 4):
+            col_check = col - i * direction[0]
+            row_check = row - i * direction[1]
+            if 0 > col_check or col_check >= self.total_cols:
+                break
+            if 0 > row_check or row_check >= len(self.board[col_check]):
+                break
+            if current_plr_pos == self.board[col_check][row_check]:
+                second_check += 1
+            else:
+                break
+
+        if first_check + second_check >= 3:
+            return True
+
+    def check_win_at(self, col: int, row: int) -> bool:
+
+        direction_list = [(-1, 0), (-1, -1), (0, -1), (1, -1)]  # left, left-bottom, bottom, right-bottom
+
+        for direction in direction_list:
+            if self.check_direction_win(col, row, direction):
+                return True
+        return False
+
+        # if current_plr_pos == self.board[col - 1][row]:  # check 1 step to the left
+        #     if current_plr_pos == self.board[col - 2][row]:  # check 2 steps to the left
+        #         if current_plr_pos == self.board[col - 3][row]:  # check 3 steps to the left
+        #             return True  # 4 in a row (3 left of current)
+        #         elif current_plr_pos == self.board[col + 1][row]:  # check 2 steps to the left and 1 to the right
+        #             return True  # 4 in a row (2 left and 1 right of current)
+        #     elif current_plr_pos == self.board[col + 1][row]:  # check 1 step to the left and 1 step to the right
+        #         if current_plr_pos == self.board[col + 2][row]:  # check 1 step to the left and 2 steps to the right
+        #             return True  # 4 in a row (1 left and 2 right of current)
+        # elif current_plr_pos == self.board[col + 1][row]:  # check 1 step to the right
+        #     if current_plr_pos == self.board[col + 2][row]:  # check 2 steps to the right
+        #         if current_plr_pos == self.board[col + 3][row]:  # check 3 steps to the right
+        #             return True  # 4 in a row (3 right of current)
+        #         elif current_plr_pos == self.board[col - 1][row]:  # check 2 steps to the right and 1 to the left
+        #             return True  # 4 in a row (2 right and 1 left of current)
+        #     elif current_plr_pos == self.board[col - 1][row]:  # check 1 step to the right and 1 step to the left
+        #         if current_plr_pos == self.board[col - 2][row]:  # check 1 step to the right and 2 steps to the left
+        #             return True  # 4 in a row (1 right and 2 left of current)
+        #     else:
+        #         return False
