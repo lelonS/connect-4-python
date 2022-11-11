1af79fea741e600020821d63eb47240e76c5c4e3
lelonS <35070147+lelonS@users.noreply.github.com>
2022-11-11 13:30:09 +0100

    Change font

--- drawer.py
+++ drawer.py
@@ -64,7 +64,7 @@ def draw_pieces(screen: pygame.Surface, board: list[list[int]]):
 
 
 def draw_text(screen: pygame.Surface, text: str, size: int, x: int, y: int, color: tuple):
-    font = pygame.font.SysFont("comicsansms", size)
+    font = pygame.font.SysFont("consolas", size)
     text = font.render(text, True, color)
     screen.blit(text, (x, y))
 
