b1d08579450b7244db4bad5ea6b47ec639b7568c
lelonS <35070147+lelonS@users.noreply.github.com>
2022-11-17 15:53:03 +0100

    Tests for legal_move

--- tests/test_connect4.py
+++ tests/test_connect4.py
@@ -1,7 +1,6 @@
 import pytest
-
-import classes.connect4
 from classes.connect4 import ConnectFour
+
 
 test_win_board1 = [
     [0, 0, 0, 0, 0, 1],
@@ -21,6 +20,14 @@ test_win_data = [
     (3, 5, LEFT, True)
 ]
 
+test_legal_move_data = [
+    (None, 0, True),
+    (None, -1, False),
+    (None, 7, False),
+    (None, 6, True),
+    (test_win_board1, 0, False)
+]
+
 
 @pytest.mark.parametrize("col, row, direction, output", test_win_data)
 def test_check_win_dir(col, row, direction, output):
@@ -29,8 +36,12 @@ def test_check_win_dir(col, row, direction, output):
     assert c.check_win_dir(col, row, direction) == output
 
 
-def test_is_legal_move():
-    pass
+@pytest.mark.parametrize("board, col, output", test_legal_move_data)
+def test_is_legal_move(board, col, output):
+    c = ConnectFour(7, 6)
+    if board is not None:
+        c.board = board
+    assert c.is_legal_move(col) == output
 
 
 def check_board_full():
