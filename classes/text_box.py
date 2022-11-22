d66691a4043151f8bc22dff80a38e5adb2b46387
Teddymannen <teddy_kross@hotmail.com>
2022-11-22 10:25:48 +0100

    s

--- classes/text_box.py
+++ classes/text_box.py
@@ -44,7 +44,7 @@ class TextBox:
 
 if __name__ == '__main__':
     pygame.init()
-    screen = pygame.display.set_mode((500, 500))
+    s = pygame.display.set_mode((500, 500))
     pygame.display.set_caption("TextBox")
 
     text_box = TextBox(100, 100, 200, 50, "PLAYER1", 15)
@@ -54,8 +54,8 @@ if __name__ == '__main__':
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-        screen.fill((0, 0, 0))
-        text_box.draw(screen)
+        s.fill((0, 0, 0))
+        text_box.draw(s)
         pygame.display.update()
 
     pygame.quit()
