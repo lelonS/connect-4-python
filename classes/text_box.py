00eb6f7c6445ef88147adb86684b870c9959e386
Teddymannen <teddy_kross@hotmail.com>
2022-11-24 14:07:42 +0100

    More textboxes, update textboxes, focus colors and some restrictions

--- classes/text_box.py
+++ classes/text_box.py
@@ -1,5 +1,8 @@
 import pygame
 
+COLOR_INACTIVE = (141, 182, 205)  # LightSkyBlue3
+COLOR_ACTIVE = (28, 134, 238)  # DodgerBlue2
+
 
 class TextBox:
     text: str
@@ -12,17 +15,17 @@ class TextBox:
     font_size: int
     max_chars: int
     is_focused: bool
-    default_text: str
+    default_text: str = "player1"
     default_text_color: tuple[int, int, int]
 
     def __init__(self, x: int, y: int, width: int, height: int, default_text: str, max_chars: int):
         self.text = ""
         self.rect = pygame.Rect(x, y, width, height)
         self.text_color = (0, 0, 0)
-        self.bg_color = (255, 255, 255)
+        self.bg_color = COLOR_INACTIVE
         self.border_color = (0, 0, 0)
         self.border_width = 2
-        self.font_size = height
+        self.font_size = height - 10
         self.font = pygame.font.SysFont("consolas", self.font_size)
         self.max_chars = max_chars
         self.is_focused = False
@@ -35,11 +38,31 @@ class TextBox:
         # Draw border
         pygame.draw.rect(screen, self.border_color, self.rect, self.border_width)
         # Draw text
-        if self.text == "":
+        if self.text == "" and not self.is_focused:
             text = self.font.render(self.default_text, True, self.default_text_color)
         else:
-            text = self.font.render(self.text, True, self.text_color)
-        screen.blit(text, (self.rect.x + 5, self.rect.y + 5))
+            if self.is_focused:
+                text = self.font.render(self.text + "<", True, self.text_color)
+            else:
+                text = self.font.render(self.text, True, self.text_color)
+        screen.blit(text, (self.rect.x + 10, self.rect.y + 10))
+
+    def update(self, event: pygame.event.Event):
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if self.rect.collidepoint(event.pos):
+                self.is_focused = True
+                self.bg_color = COLOR_ACTIVE
+            else:
+                self.is_focused = False
+                self.bg_color = COLOR_INACTIVE
+        if event.type == pygame.KEYDOWN and self.is_focused:
+            if event.key == pygame.K_BACKSPACE:
+                self.text = self.text[:-1]
+            elif event.key == pygame.K_RETURN:
+                self.is_focused = False
+            elif len(self.text) < self.max_chars:
+                if event.unicode.upper() in "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789":
+                    self.text += event.unicode.upper()
 
 
 if __name__ == '__main__':
@@ -47,15 +70,23 @@ if __name__ == '__main__':
     s = pygame.display.set_mode((500, 500))
     pygame.display.set_caption("TextBox")
 
-    text_box = TextBox(100, 100, 200, 50, "PLAYER1", 15)
+    text_boxes = [
+        TextBox(100, 100, 200, 50, "PLAYER1", 7),
+        TextBox(100, 200, 200, 50, "PLAYER2", 7),
+        TextBox(100, 300, 200, 50, "PLAYER3", 7),
+        TextBox(100, 400, 200, 50, "PLAYER4", 7)
+    ]
 
     run = True
     while run:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+        for e in pygame.event.get():
+            if e.type == pygame.QUIT:
                 run = False
-        s.fill((0, 0, 0))
-        text_box.draw(s)
+            for textbox in text_boxes:
+                textbox.update(e)
+        s.fill((100, 100, 100))
+        for textbox in text_boxes:
+            textbox.draw(s)
         pygame.display.update()
 
     pygame.quit()
