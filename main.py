b644f6abdf7e4bdf60e16c0e32b8f3345a5a78e2
Teddymannen <teddy_kross@hotmail.com>
2022-11-25 09:59:09 +0100

    add temp menu

--- main.py
+++ main.py
@@ -1,18 +1,46 @@
 import pygame
 from constants import WIDTH, HEIGHT
 from game_screen import GameScreen
+from classes.button import Button
+
+pygame.init()
+
+# Create screen
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Connect4")
+
+
+def start_game():
+    game_screen = GameScreen(screen, (0, 600), 7, 6, [])
+    game_screen.run_game()
 
 
 def main():
-    pygame.init()
+    # Play button
+    play_button = Button(325, 200, 250, 100, "Play", start_game)
+    play_button.bg_color = (0, 0, 0)
+    play_button.text_color = (255, 255, 255)
 
-    # Create screen
-    screen = pygame.display.set_mode((WIDTH, HEIGHT))
-    pygame.display.set_caption("Connect4")
+    # Main menu
+    while True:
+        # Draw background
+        screen.fill((0, 0, 0))
+
+        # Draw buttons
+        play_button.draw(screen)
+
+        # Update screen
+        pygame.display.update()
+
+        # Check events
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+            play_button.update(event)
 
     # Open game screen
-    game_screen = GameScreen(screen, (0, 600), 7, 6, [])
-    game_screen.run_game()
+    # game_screen = GameScreen(screen, (0, 600), 7, 6, [])
+    # game_screen.run_game()
 
 
 if __name__ == '__main__':
