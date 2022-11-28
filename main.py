c60cb15de889a89497831f3637f3cea92ef3533e
lelonS <35070147+lelonS@users.noreply.github.com>
2022-11-28 10:30:11 +0100

    Update main to use scenemanager

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 from constants import WIDTH, HEIGHT, BOARD_BOTTOM_LEFT
 from game_scene import GameScene
 from classes.button import Button
+from classes.scene import Scene, SceneManager
 
 pygame.init()
 
@@ -16,28 +17,31 @@ def start_game():
 
 
 def main():
-    # Play button
-    play_button = Button(325, 200, 250, 100, "PLAY", start_game)
-    play_button.bg_color = (0, 0, 0)
-    play_button.text_color = (255, 255, 255)
+
+    # Create scene manager
+    sceneManager = SceneManager()
+    sceneManager.add_scene(GameScene(screen, BOARD_BOTTOM_LEFT, 7, 6, []))
+
+    # Create clock
+    clock = pygame.time.Clock()
 
     # Main menu
     while True:
-        # Draw background
-        screen.fill((0, 0, 0))
 
-        # Draw buttons
-        play_button.draw(screen)
-
-        # Update screen
-        pygame.display.update()
+        # Get clock info
+        ms = clock.tick()
+        seconds = ms / 1000
 
         # Check events
-        for event in pygame.event.get():
+        events = pygame.event.get()
+        for event in events:
             if event.type == pygame.QUIT:
                 pygame.quit()
                 exit()
-            play_button.update(event)
+        sceneManager.update(events, seconds)
+
+        # Draw
+        sceneManager.draw()
 
     # Open game screen
     # game_screen = GameScreen(screen, (0, 600), 7, 6, [])
