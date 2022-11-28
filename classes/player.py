100bcd4adfbe17edf32d69b443f621dc7b2405b8
Teddymannen <teddy_kross@hotmail.com>
2022-11-28 09:36:41 +0100

    add variables and init to player class

--- classes/player.py
+++ classes/player.py
@@ -1,2 +1,15 @@
 class Player:
     name: str
+    color: tuple  # RGB
+    wins: int
+    ties: int
+    losses: int
+    games: int  # wins + ties + losses
+
+    def __init__(self, name: str, color: tuple) -> None:
+        self.name = name
+        self.color = color
+        self.wins = 0
+        self.ties = 0
+        self.losses = 0
+        self.games = 0
