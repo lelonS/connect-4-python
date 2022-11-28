41011d9ba8853359d4893b6721a46ad334c19ad2
lelonS <35070147+lelonS@users.noreply.github.com>
2022-11-28 10:29:45 +0100

    Create scene.py

---
+++ classes/scene.py
@@ -0,0 +1,40 @@
+import pygame
+
+
+class Scene:
+    screen: pygame.Surface
+
+    def __init__(self, screen: pygame.Surface) -> None:
+        self.screen = screen
+
+    def update(events: list[pygame.event.Event], dt: float, sceneManager: 'SceneManager'):
+        raise NotImplementedError
+
+    def draw():
+        raise NotImplementedError
+
+
+class SceneManager:
+    scenes: list[Scene]
+
+    def __init__(self):
+        self.scenes = []
+
+    def add_scene(self, scene):
+        self.scenes.append(scene)
+
+    def go_back(self):
+        if len(self.scenes) > 0:
+            del self.scenes[-1]
+
+    def go_to_origin_scene(self):
+        while len(self.scenes) > 1:
+            self.go_back()
+
+    def update(self, events: list[pygame.event.Event], dt: float):
+        if len(self.scenes) > 0:
+            self.scenes[-1].update(events, dt, self)
+
+    def draw(self):
+        if len(self.scenes) > 0:
+            self.scenes[-1].draw()
