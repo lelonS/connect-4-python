4e10aa3512f91e409a4834b10158e34f9ebfb95a
Teddymannen <teddy_kross@hotmail.com>
2022-11-30 09:59:04 +0100

    fix play button

--- main_menu.py
+++ main_menu.py
@@ -15,20 +15,26 @@ class MainMenu(Scene):
 
     def __init__(self, screen: pygame.Surface):
         super().__init__(screen)
-        self.col_buttons = IntSelector(100, 100, 50, 50, 3, 3, 10)
-        self.row_buttons = IntSelector(100, 200, 50, 50, 3, 3, 10)
+        self.col_buttons = IntSelector(100, 100, 50, 50, 7, 7, 10)
+        self.row_buttons = IntSelector(100, 200, 50, 50, 6, 6, 10)
         self.player_number_buttons = IntSelector(100, 300, 50, 50, 2, 2, 4)
         self.player_text_boxes = [TextBox(100, 400 + i * 50, 200, 50, f'Player {i + 1}', 7) for i in range(4)]
         self.play_button = Button(100, 500, 200, 50, 'Play', self.play)
 
     def play(self):
-        game_scene = GameScene(self.screen, (100, 600), self.col_buttons.value, self.row_buttons.value, [])
+        game_scene = GameScene(self.screen, (0, 600), self.col_buttons.value, self.row_buttons.value, [])
         self.scene_manager.add_scene(game_scene)
 
     def update(self, events: list[pygame.event.Event], seconds: float, scene_manager: SceneManager):
         self.scene_manager = scene_manager
+        for event in events:
+            self.col_buttons.update(event)
+            self.row_buttons.update(event)
+            self.play_button.update(event)
 
     def draw(self):
         self.screen.fill((0, 0, 0))
+        self.col_buttons.draw(self.screen)
+        self.row_buttons.draw(self.screen)
         self.play_button.draw(self.screen)
         pygame.display.update()
