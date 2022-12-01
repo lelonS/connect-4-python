6ffc612652dce0c019147fb405692bf4892fba1f
lelonS <35070147+lelonS@users.noreply.github.com>
2022-12-01 17:15:22 +0100

    Add docstrings

--- classes/scene.py
+++ classes/scene.py
@@ -20,21 +20,38 @@ class SceneManager:
     def __init__(self):
         self.scenes = []
 
-    def add_scene(self, scene):
+    def add_scene(self, scene: Scene):
+        """Adds a scene, and goes to it
+
+        Args:
+            scene (Scene): The scene to add
+        """
         self.scenes.append(scene)
 
     def go_back(self):
+        """Goes back to the previous scene
+        """
         if len(self.scenes) > 0:
             del self.scenes[-1]
 
     def go_to_origin_scene(self):
+        """Goes to the first scene (the origin scene)
+        """
         while len(self.scenes) > 1:
             self.go_back()
 
     def update(self, events: list[pygame.event.Event], dt: float):
+        """Updates the current scene
+
+        Args:
+            events (list[pygame.event.Event]): All events except a quit event
+            dt (float): Time since last update in seconds
+        """
         if len(self.scenes) > 0:
             self.scenes[-1].update(events, dt, self)
 
     def draw(self):
+        """Draws the current scene
+        """
         if len(self.scenes) > 0:
             self.scenes[-1].draw()
