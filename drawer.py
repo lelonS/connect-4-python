986d82370d8b82b8e74628776969d920786bf2c5
lelonS <35070147+lelonS@users.noreply.github.com>
2022-12-06 13:14:56 +0100

    Delete drawer.py

--- drawer.py
+++
@@ -1,8 +0,0 @@
-import pygame
-from constants import FONT_PATH
-
-
-def draw_text(screen: pygame.Surface, text: str, size: int, x: int, y: int, color: tuple):
-    font = pygame.font.Font(FONT_PATH, size)
-    text = font.render(text, True, color)
-    screen.blit(text, (x, y))
